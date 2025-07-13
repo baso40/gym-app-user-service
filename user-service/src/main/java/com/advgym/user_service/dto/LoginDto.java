4783d10b8ccc3611282ea103563c22a2736ee412
baso40 <111798358+baso40@users.noreply.github.com>
2025-07-13 23:32:14 +0530

    Update LoginDto.java

--- user-service/src/main/java/com/advgym/user_service/dto/LoginDto.java
+++ user-service/src/main/java/com/advgym/user_service/dto/LoginDto.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginDto {
-	@NotBlank(message = "Email is required")
+    @NotBlank(message = "Email is required")
     @Email(message = "Email should be valid")
     private String email;
     
